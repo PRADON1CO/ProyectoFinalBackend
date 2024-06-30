33282d8aad28bf179c97f2dcc580fbaef4c9cbf6
Prado Nicolas <nicolas88prado@gmail.com>
2024-06-30 11:10:01 -0300

    agregue configuracion de la variable de entorno

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('prueba')
+import express from "express"
+
+//1- configuro un puerto
+const app = express();
+//creo una variable
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port') ,()=>{
+    console.log('Estoy escuchando el purto ' + app.get(('port')))
+})
+//2- Configurar los middlewares
+
+
+//3- creo las rutas
