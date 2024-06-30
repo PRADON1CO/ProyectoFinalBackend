b6103eed5ebc953911c6eea87758205cc4a9af53
Prado Nicolas <nicolas88prado@gmail.com>
2024-06-30 18:12:01 -0300

    agregue la logica para la funcion get para obtener una clase y controle dos tipos de errores que puedan suceder

--- src/routes/clases.routes.js
+++ src/routes/clases.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { CrearClase, listarClases } from "../controllers/clases.controllers.js";
+import { CrearClase, listarClases, obtenerClase } from "../controllers/clases.controllers.js";
 
 const router = Router();
 
 //creo rutas
 router.route('/prueba').get(listarClases);
 router.route('/clases').post(CrearClase).get(listarClases).put().delete()
+router.route('/clases/:id').get(obtenerClase)
 
 export default router
