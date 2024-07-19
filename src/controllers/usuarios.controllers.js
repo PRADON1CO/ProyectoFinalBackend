a2238cb21eb367b4a39ba1fee869439e7ce551b4
Prado Nicolas <nicolas88prado@gmail.com>
2024-07-18 21:23:10 -0300

    agregue la funcion de login

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -24,3 +24,27 @@ export const crearUsuario = async (req, res) => {
         res.status(400).json({mensaje: 'Ocurrio un error al intentar crear un usuario'})
     }
 }
+
+export const login = async (req, res) => {
+    try{
+        // verifico si el email ya fue registrado
+        const {email, password, nombreUsuario} = req.body
+        // findOne({email}) equivale o es lo mismo que findOne({email: req.body.email})
+        const usuarioExistente = await Usuario.findOne({email}) 
+        if(!usuarioExistente){
+            return res.status(400).json({memsaje: 'Correo o pasasword incorrecto -email'});
+        }
+        // verificar el password
+        const passwordValido = bcrypt.compareSync(password, usuarioExistente.password)
+        // quiero saber si el password es correcto
+        if(!passwordValido){
+            return res.status(400).json({memsaje: 'Correo o pasasword incorrecto -password'})
+        }
+        // enviar respuesta afirmativa
+        res.status(200).json({memsaje: 'Los datos del usuario son validos'})
+        
+    }catch(error){
+        console.error(error);
+        res.status(500).json({mensaje: 'Ocurrio un error al intentar logiar un usuario'});
+    }
+}
